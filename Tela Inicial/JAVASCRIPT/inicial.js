408b158d87b0b05e37dce7ede0b37497ed5779a8
lucasrossi0 <69816959+lucasrossi0@users.noreply.github.com>
2020-11-20 14:22:06 -0300

    inicial

---
+++ Tela Inicial/JAVASCRIPT/inicial.js
@@ -0,0 +1,13 @@
+var mapa;
+function initMap() {
+  var configuracoes = {
+    center: {lat: -22.897401, lng: -43.261232},
+    zoom: 15
+  }
+  mapa = new google.maps.Map(document.getElementById('map'), configuracoes);
+  var marcador = new google.maps.Marker({
+    position: {lat: -22.897401, lng: -43.261232},
+    title: "COMPLEXO DO RATO MOLHADO",
+    map: mapa
+  });
+}
