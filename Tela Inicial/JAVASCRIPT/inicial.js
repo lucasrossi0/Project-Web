ab1bddb5586453b4aff4bb004cd7c399a5c1a142
lucasrossi0 <69816959+lucasrossi0@users.noreply.github.com>
2020-11-21 16:54:26 -0300

    main

--- Tela Inicial/JAVASCRIPT/inicial.js
+++ Tela Inicial/JAVASCRIPT/inicial.js
@@ -2,7 +2,7 @@ var mapa;
 function initMap() {
   var configuracoes = {
     center: {lat: -22.897401, lng: -43.261232},
-    zoom: 15
+    zoom: 10
   }
   mapa = new google.maps.Map(document.getElementById('map'), configuracoes);
   var marcador = new google.maps.Marker({
