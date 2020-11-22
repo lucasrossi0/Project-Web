9ce8f8dfd47bc9cd26ef38f85f3a13e5c61e6e5b
lucasrossi0 <69816959+lucasrossi0@users.noreply.github.com>
2020-11-22 13:53:56 -0300

    main

--- Tela Inicial/JAVASCRIPT/inicial.js
+++ Tela Inicial/JAVASCRIPT/inicial.js
@@ -1,13 +1,24 @@
 var mapa;
+
+$(document).ready(function () {
+  initMap();
+
+  $('#tProducts').click( function () {
+    window.location.href = "../Tela Produtos/HTML/telaProdutos";
+  });
+});
+
+
+
 function initMap() {
   var configuracoes = {
     center: {lat: -22.897401, lng: -43.261232},
-    zoom: 8
-  }
+    zoom: 10
+  };
   mapa = new google.maps.Map(document.getElementById('map'), configuracoes);
   var marcador = new google.maps.Marker({
     position: {lat: -22.897401, lng: -43.261232},
     title: "COMPLEXO DO RATO MOLHADO",
     map: mapa
   });
-}
+};
--- Tela de Login/JAVASCRIPT/tela_login.js
+++ Tela de Login/JAVASCRIPT/tela_login.js
@@ -6,9 +6,21 @@ $(document).ready(function () {
     $('#bEnter').click( function () {
 
         var email = $('#tEmail').val();
-        var password = $('#tRegister').val();
+        var password = $('#tSenha').val();
 
+        var pessoa = JSON.parse(localStorage.getItem('Pessoa'));
         
+        if (email == '' || password == '') {
+            alert('Preencha todos os campos');
+        } else {
+            for (a = 0; a < pessoa.length; a++) {
+                if (pessoa[a][1] == email && pessoa[a][2] == password) {
+                    window.location.href = '../../Tela Inicial/index.html';
+                } else {
+                    alert('Dados não encontrados!');
+                }
+            }
+        }
     });
     
 });
